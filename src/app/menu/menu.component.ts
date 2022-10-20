842d5dfa363bfb01037634048c8ff29b289ab40b
DanielDevOltec <116025047+DanielDevOltec@users.noreply.github.com>
2022-10-20 18:15:45 -0500

    adding category form (#3)
    v1.0.0

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+interface MenuItem {
+  path: string;
+  label: string;
+}
+
+@Component({
+  selector: 'app-menu',
+  template: `
+  <div>
+   <mat-toolbar>Menu</mat-toolbar>
+    <mat-nav-list>
+      <a mat-list-item [routerLink]="item.path" *ngFor="let item of menuItems; index as i">{{item.label}}</a>
+    </mat-nav-list>
+  </div>
+  `,
+  styles: [
+  ]
+})
+export class MenuComponent implements OnInit {
+
+  menuItems : Array<MenuItem> = [
+    {path : "/",label : "Home"},
+    {path: "/categories", label:"Categories"},
+    {path: "/suppliers", label:"Suppliers"},
+
+  ];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
