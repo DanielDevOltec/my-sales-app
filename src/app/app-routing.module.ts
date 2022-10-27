f09d753d4fffd5ad8283bf010c5887ac6f5bbf70
DanielDevOltec <116025047+DanielDevOltec@users.noreply.github.com>
2022-10-27 12:43:37 -0500

    add async loading (#4)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesLoadingAsyncComponent } from './categories/categories-loading-async/categories-loading-async.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 const routes : Routes = [
   {path:"", component: DashboardComponent},
-  {path: "categories", component: CategoriesComponent}
+  {path: "categories", component: CategoriesComponent},
+  {path: "categories-async", component: CategoriesLoadingAsyncComponent}
 ]
 @NgModule({
   declarations: [],
